b59b8825355058739b671e243b4af610f0e3148a
IgnatiyOS <ignatii1994-23-10.ru@mail.ru>
2018-04-24 12:28:18 +0700

    v_0.2
    теперь читает лог и выводит контрольную информацию перед запуском программы.. не знаю зачем сделал, но пусть будет)

--- sms.sh
+++ sms.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
+sleep 1;
+grep -E "Gammu" gammu.log
+grep -E "Connection" gammu.log
+grep -E "Model type" gammu.log
+grep -E "Device" gammu.log
+grep -E "Running on" gammu.log
+grep -E "Connected model" gammu.log
+grep -E "Firmware version" gammu.log
+grep -E "Serial device:" gammu.log
+sleep 1;
+echo "Develop: IgnatiyOS"
+sleep 1;
 
 cat /home/service/dfg | while read I
 do
